a4aed82e544cea7fa69ea2a9b9dd44c710468f7b
Piotr Zakrzewski <p.zakrzewski92@wp.pl>
2022-10-23 18:18:45 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,22 +10,21 @@ const CountryList = document.querySelector('.list-country');
 const CountryInfo = document.querySelector('.info-country');
 
 const animateCSS = (element, animation, prefix = 'animate__') =>
-  // We create a Promise and return it
+  // create a Promise and return it
   new Promise((resolve, reject) => {
     const animationName = `${prefix}${animation}`;
     const node = document.querySelector(element);
 
-    node.classList.add(`${prefix}animated`, animationName);
+        node.classList.add(`${prefix}animated`, animationName);
 
-    // When the animation ends, we clean the classes and resolve the Promise
-    function handleAnimationEnd(event) {
-      event.stopPropagation();
-      node.classList.remove(`${prefix}animated`, animationName);
-      resolve('Animation ended');
-    }
-
-    node.addEventListener('animationend', handleAnimationEnd, { once: true });
-  });
+        // When the animation ends, clean the classes and resolve the Promise
+        function handleAnimationEnd(event) {
+        event.stopPropagation();
+        node.classList.remove(`${prefix}animated`, animationName);
+        resolve('Animation ended');
+        }
+        node.addEventListener('animationend', handleAnimationEnd, { once: true });
+    });
 
 function cleanMarkup(ref) {
   ref.innerHTML = '';
